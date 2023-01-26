23bdef4e8059db06f1dd89542f927a3d5fec7058
Marco Abbondanza <107813503+Marco9811@users.noreply.github.com>
2023-01-26 19:59:18 +0100

    Initial commit

---
+++ src/Start.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class Start {
+    public static void main(String[] args) {
+        ArrayList<String> cityNames = new ArrayList<String>();
+        ArrayList<String> kingsOfRome = new ArrayList<String>();
+
+        cityNames.add("Roma");
+        cityNames.add("Parigi");
+        cityNames.add("Londra");
+        cityNames.add("Milano");
+        System.out.println(cityNames);
+
+        kingsOfRome.add("Romolo");
+        kingsOfRome.add("Numa Pompilio");
+        kingsOfRome.add("Tullo Ostilio");
+        kingsOfRome.add("Anco Marcio");
+        kingsOfRome.add("Tarquinio Prisco");
+        kingsOfRome.add("Servio Tullio");
+        kingsOfRome.add("Tarquinio il Superbo");
+        System.out.println(kingsOfRome);
+        for(int i=0; i< kingsOfRome.add(); i++){
+            System.out.println(kingsOfRome);
+        }
+    }
+
+}
